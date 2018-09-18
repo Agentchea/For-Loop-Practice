cd0d01b74a44a962882e8eec7716433221becaa7
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-18 15:37:11 -0400

    Add files via upload

---
+++ forlooppracticeV2.js
@@ -0,0 +1,26 @@
+var x = 60;
+
+while (x > 0) {
+    if (x === 1) {
+        console.log("T-" + x + " seconds");
+
+    }
+    if (x === 1) {
+        console.log("T-" + x <= 57 + "..." );
+    }
+
+    if (x === 1){
+        console.log("T-" + x == 51 + " Orbiter tranfers from grounf to internal power " + " seconds");
+    }
+
+     if (x === 1) {
+        console.log("T-" + x <= 49 + "..." );
+    }
+     if (x === 1) {
+        console.log("T-" + x <= 3 + " seconds" );
+    }
+    if (x === 1) {
+        console.log("T-" + x === 0 + " Solid rocket booster ignition and liftoff! " + " seconds");
+    }
+    x = x -1;
+}
