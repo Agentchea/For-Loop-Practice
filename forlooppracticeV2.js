10cbcc2a4372d2800a615f9bac7029804639f4d6
Derrick Chea <derrick.c.chea@gmail.com>
2018-09-18 15:38:12 -0400

    Add files via upload

--- forlooppracticeV2.js
+++ forlooppracticeV2.js
@@ -1,26 +1,34 @@
-var x = 60;
+var num = 60;
 
-while (x > 0) {
-    if (x === 1) {
-        console.log("T-" + x + " seconds");
-
-    }
-    if (x === 1) {
-        console.log("T-" + x <= 57 + "..." );
+while (num >= 0) {
+    if (num === 50) {
+        console.log("Orbiter transfers from ground to internal power" + "T-" + num + " seconds");
     }
 
-    if (x === 1){
-        console.log("T-" + x == 51 + " Orbiter tranfers from grounf to internal power " + " seconds");
+    else if (num === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start" + "T-" + num + " seconds");
     }
 
-     if (x === 1) {
-        console.log("T-" + x <= 49 + "..." );
+    else if (num === 16) {
+        console.log("Activate launch pad sound suppression system " + "T-" + num + "seconds");
     }
-     if (x === 1) {
-        console.log("T-" + x <= 3 + " seconds" );
+
+    else if (num === 10) {
+        console.log("Activate main engine hydrogen burnoff system" + "T-" + num + "seconds");
     }
-    if (x === 1) {
-        console.log("T-" + x === 0 + " Solid rocket booster ignition and liftoff! " + " seconds");
+
+    else if (num === 6) {
+        console.log("Main engine start " + "T-" + num + "seconds");
     }
-    x = x -1;
+
+    else if (num === 0){
+        console.log("Solid rocket booster ignition and liftoff! " + "T-" + num + "seconds");
+    }
+
+    else {
+        console.log("T-" + num + "seconds");
+    }
+    num--;
 }
+
+ 
